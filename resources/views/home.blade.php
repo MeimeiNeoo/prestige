7bf8b2fdfb094d2523e8a8a89816449cb891a413
DewiNeo <dewi.ismaneti@binus.ac.id>
2023-07-05 21:36:37 +0800

    add collaboration section and paginate

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -336,6 +336,59 @@
     </section>
 
 
+    
+    {{-- why you should choose prestige  --}}
+    <section class="collab" id="collab">    
+        <div class="brand">
+            <h1 class="contentTitle">BRAND WE WORK WITH</h1>   
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                  <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1">Previous</a>
+                  </li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item">
+                    <a class="page-link" href="#">Next</a>
+                  </li>
+                </ul>
+            </nav>
+        </div>  
+        <div class="influencer">
+            <h1 class="contentTitle">INFLUENCER WE COLLAB WITH</h1>    
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                  <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1">Previous</a>
+                  </li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item">
+                    <a class="page-link" href="#">Next</a>
+                  </li>
+                </ul>
+            </nav> 
+        </div>  
+        <div class="mediaPartner">
+            <h1 class="contentTitle">MEDIA PARTNER</h1> 
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                  <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1">Previous</a>
+                  </li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item">
+                    <a class="page-link" href="#">Next</a>
+                  </li>
+                </ul>
+            </nav>
+        </div>
+    </section>
+
     {{-- Contact section  --}}
     {{-- ini bagian background --}}
     {{-- <section class="contact section" id="contact">
